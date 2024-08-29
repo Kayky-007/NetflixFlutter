3dccc100da72e33d4b8d6c0e3c722ea9c33446ac
Kayky <149194790+Kayky-007@users.noreply.github.com>
2024-08-29 13:31:08 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,20 +2,7 @@ import 'package:estudo1/components/capaFilme.dart';
 import 'package:estudo1/components/espacamento_h.dart';
 import 'package:flutter/material.dart';
 
-// Supondo que a classe CapaFilme esteja definida em outro arquivo como:
-// class CapaFilme extends StatelessWidget {
-//   final String imagemCapa;
-//
-//   const CapaFilme({Key? key, required this.imagemCapa}) : super(key: key);
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       margin: const EdgeInsets.all(8.0),
-//       child: Image.asset(imagemCapa),
-//     );
-//   }
-// }
+
 
 void main() => runApp(const MyApp());
 
